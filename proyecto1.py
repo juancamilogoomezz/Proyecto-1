b8c2cbbcdc5e2b582c226af70a6dd60a74204797
juancamilogoomezz <j.gomezu@uniandes.edu.co>
2026-02-15 14:58:21 -0500

    ya con los datos

--- proyecto1.py
+++ proyecto1.py
@@ -2,4 +2,15 @@
 #Juan Camilo Gómez-202220238
 #Jerónimo Rueda-202223775
 
+import numpy as np
+import pandas as pd 
+
+df = pd.read_csv("Saber 11 Datos Valle.csv")
+
+#print(df.shape)
+
+#(642592, 51)
+
+
+print(df.isna().sum())
 
